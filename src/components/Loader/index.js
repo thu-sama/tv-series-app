8e9a446d54316f5478e39b4f0ccc1034bcbad3e9
Tun Lin Thu <thusama@Tuns-MacBook-Pro.local>
2018-04-28 13:00:05 +0900

    Init

---
+++ src/components/Loader/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import loader from "../../assets/loader.gif";
+
+const Loader = props => (
+	<div>
+		<img src={loader} alt="Loader icon" style={{ marginTop: 20 }} />
+	</div>
+);
+
+export default Loader;
